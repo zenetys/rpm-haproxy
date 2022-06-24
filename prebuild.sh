ea2996cfe30c9f864a3db8a2e4fee3b22dfc6ed3
Julien Thomas <jthomas@zenetys.com>
2022-06-24 10:24:35 +0200

    BUILD: Update rpmbuild-docker el6 build image to use CentOS vault repos
    CentOS 6 is EOL, standard YUM repos are not available anymore in mirrors.
    This patch updates the docker base image used for el6 builds so it points
    to the archive repos at vault.centos.org. As a consequence, the
    prebuild.sh script gets removed since it was doing the same thing. The
    difference is that the change in rpmbuild-docker will trigger the build
    of a new el6 base image next time rpmbuild-docker -d el6 is run.

--- prebuild.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash -xe
-
-case "$DIST" in
-    el6)
-        # el6 EOL, use archive repositories
-        sed -i -re 's,mirror\.centos\.org,vault.centos.org,; s,^(mirrorlist),#\1,; s,^#(baseurl),\1,' \
-            /etc/yum.repos.d/CentOS-Base.repo
-        ;;
-esac
